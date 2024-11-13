24eefc6d565c4f230c02d234b6fe9ad530b0c620
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2024-11-13 13:24:21 +0100

    helpers: Refactor `Yield` future and add documentation
    Reorganized import statements and provided detailed documentation for the `Yield` struct and its methods. This enhances code readability and ensures proper understanding of how the `Yield` future operates and its usage.

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,17 +1,29 @@
+use core::default::Default;
 use core::future::Future;
 use core::pin::Pin;
 use core::task::{Context, Poll};
-use core::default::Default;
 
-
+/// A struct that implements the `Future` trait to create a single-yield future.
 #[derive(Default)]
 struct Yield {
+    /// A flag indicating whether the future has yielded once.
     flag: bool,
 }
 
 impl Future for Yield {
     type Output = ();
 
+    /// Polls the future to determine if it is ready.
+    ///
+    /// # Parameters
+    ///
+    /// * `cx`:
+    ///   A mutable reference to the task's context, used to wake up the task when it is ready to make progress.
+    ///
+    /// # Returns
+    ///
+    /// * `Poll::Ready(())` if the future has already yielded once and is now ready.
+    /// * `Poll::Pending` if the future needs to yield.
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         if self.flag {
             return Poll::Ready(());
@@ -23,6 +35,10 @@ impl Future for Yield {
     }
 }
 
+/// Asynchronously yields execution back to the executor.
+///
+/// This function creates an instance of the `Yield` future and awaits its completion,
+/// effectively yielding execution back to the executor once.
 pub async fn yield_me() {
     Yield::default().await;
 }
