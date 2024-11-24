f3f41b18de206701e79f4defa657d8719701bcf2
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2024-11-24 20:12:57 +0100

    miniloop: Add build script to generate task array size constant
    This commit introduces a `build.rs` script that generates a file containing a constant for task array size. The task array size is derived from the environment variable `MINILOOP_TASK_ARRAY_SIZE`, defaulting to '1' if not set, and is written to `OUT_DIR` during the build process. The script ensures that Cargo reruns the build script if `build.rs` itself changes.

---
+++ build.rs
@@ -0,0 +1,16 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+
+fn main() {
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+    let task_array_size = env::var("MINILOOP_TASK_ARRAY_SIZE").unwrap_or(String::from("1"));
+    let dest_path = Path::new(&out_dir).join("task_array_size.inc");
+
+    fs::write(
+        &dest_path,
+        format!("const TASK_ARRAY_SIZE: usize = {task_array_size};\n"),
+    )
+    .unwrap();
+    println!("cargo::rerun-if-changed=build.rs");
+}
